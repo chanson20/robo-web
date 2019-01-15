36b3759d656e0e16e96f4ad7bab15853b5f9f173
Cooper Hanson <chanson20@bsmschool.org>
2019-01-15 10:43:53 -0600

    update
    added imported modules

--- basic.py
+++ basic.py
@@ -1,1 +1,8 @@
-#empty
+#    Add this code to index.html in the robo-html directory
+#    <img src="https://192.168.21.XXX:8080/?action=stream">
+###
+#    start camera with /home/student/start_camera.sh
+
+import RoboPiLib as RPL
+import setup
+import post_to_web.py as PTW
